3d463b8445d6af72b31b2f8f5d03322d71fb8943
Jonathan Alvarez <j@jonalvarezz.com>
2022-09-21 21:05:42 +0200

    completes update route
    Signed-off-by: Jonathan Alvarez <j@jonalvarezz.com>

--- api/routes/profile/update-profile.js
+++ api/routes/profile/update-profile.js
@@ -1,17 +1,54 @@
 import { Router } from 'express';
 import { body, check, validationResult } from 'express-validator';
+
+import { hashPassword } from '../../helpers/auth.js';
 import { UserModel } from '../../models/User.js';
 
 export const updateUser = Router();
 
 updateUser.put(
   '/',
-  // @todo: Validación y sanitización de los datos de entrada
+  // Validación y sanitización de los datos de entrada
+  body('username').not().isEmpty().trim(),
+  check('username').custom(async (username) => {
+    const maybeUser = await UserModel.findOne({ username });
+    if (maybeUser) {
+      throw new Error('username already in use');
+    }
 
-  // @todo: Actualizar información usuario según la sesión del token JWT
+    return true;
+  }),
+
+  //  Actualizar información usuario según la sesión del token JWT
   async (request, response) => {
-    return response.status(200).json({
-      //
-    });
+    try {
+      const errors = validationResult(request);
+      if (!errors.isEmpty()) {
+        return response.status(400).json({ errors: errors.array() });
+      }
+
+      const user = request.user;
+
+      const password = await hashPassword(request.body.password);
+
+      await UserModel.findByIdAndUpdate(user.id, {
+        username: request.body.username,
+        password: password,
+      });
+      // force get update
+      const newUser = await UserModel.findById(user.id);
+
+      return response.status(200).json({
+        username: newUser.username,
+        createdAt: newUser.createdAt,
+        updatedAt: newUser.updatedAt,
+      });
+    } catch (error) {
+      console.debug(`[updateUser]: ${error}`);
+
+      return response.status(500).json({
+        error: 'An unexpected error happened. Please try again later',
+      });
+    }
   }
 );
