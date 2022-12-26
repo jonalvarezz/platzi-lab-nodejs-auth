aac43b498eb2820c4a9a5891305c64d0672a07cf
Pedro Andres Chaparro Quintero <pedroandreschaparro@gmail.com>
2022-12-26 15:47:06 -0500

    :sparkles: `/profile` get user information from database

--- api/routes/profile/view-profile.js
+++ api/routes/profile/view-profile.js
@@ -1,17 +1,19 @@
 import { Router } from 'express';
-import { body, check, validationResult } from 'express-validator';
 import { UserModel } from '../../models/User.js';
 
 export const viewUser = Router();
 
 viewUser.get(
   '/',
-  // @todo: Validación y sanitización de los datos de entrada
-
-  // @todo: Ver información del usuario actual según la sesión del token JWT
   async (request, response) => {
-    return response.status(200).json({
-      user: request.user
-    });
+    const { _id } = request.user;
+    const user = await UserModel.findById(_id);
+
+    if (!user)
+      return response
+        .status(404)
+        .json({ error: true, message: 'User was not found' });
+
+    return response.status(200).json({ _id, user: user['username'] });
   }
 );
